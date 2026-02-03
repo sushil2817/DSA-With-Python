5674dddd310a7a95ae4adc65008de28c22beffc7
Sushil Kumawat <sushil.kumawat@ubiqedge.com>
2026-02-03 19:19:10 +0530

    Learn about array in python

--- Python First/Array.py
+++ Python First/Array.py
@@ -1,3 +1,20 @@
-import array
+from array import *
 
-val = array.array('i',[1,2,3,4,5,6])
+val = array('i',[1,2,3,4,5,6,7,8,9,10])
+
+print('\n')
+for i in range(0,len(val)):
+    print(val[i],end=" ")
+
+print('\n')
+for x in val:
+    print(x,end=",")
+
+print('\n')
+
+print(val.typecode)
+
+val.reverse()
+for i in val:
+    print(i,end=',')
+
