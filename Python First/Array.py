84c1293ea8b649b474e6ba8156a84139b051ca5f
sushil2817 <tony281723@gmail.com>
2026-02-03 22:56:09 +0530

    learn about multi dimentional arrays

--- Python First/Array.py
+++ Python First/Array.py
@@ -1,34 +1,50 @@
 from array import *
 
-val = array('i',[1,2,3,4,5,6,7,8,9,10])
+# val = array('i',[1,2,3,4,5,6,7,8,9,10])
 
-print('\n')
-for i in range(0,len(val)):
-    print(val[i],end=" ")
+# print('\n')
+# for i in range(0,len(val)):
+#     print(val[i],end=" ")
 
-print('\n')
-for x in val:
-    print(x,end=",")
+# print('\n')
+# for x in val:
+#     print(x,end=",")
 
-print('\n')
+# print('\n')
 
-print(val.typecode)
+# print(val.typecode)
 
-# val.reverse()
+# # val.reverse()
+# # for i in val:
+# #     print(i,end=',')
+
+# val.insert(1,50)
+# val.append(100)
+# val[2] = 200
+
+# copyArray = array(val.typecode, (x*3 for x in val))
+
+
 # for i in val:
-#     print(i,end=',')
+#     print (i,end=",")
+# print('\n')
+# # copyArray.pop()
 
-val.insert(1,50)
-val.append(100)
-val[2] = 200
+# abc = val[::-1]
 
-copyArray = array(val.typecode, (x*3 for x in val))
+# for i in abc:
+#     print (i,end=",")
 
 
-for i in copyArray:
-    print (i,end=",")
-print('\n')
-copyArray.pop()
+arr = array('i',[12,1,3,1,4,1,5,16,16,17,18])
 
-for i in copyArray:
-    print (i,end=",")
+# n = int(input('Enter a Number'))
+
+# for i in range(0,n):
+#     arr.append(int(input('Enter next input')))
+
+# for i in range(0,n):
+#     print(arr[i],end=",")
+
+index = arr.index(1);
+print(index)
---
+++ Python First/ArrayNumpy.py
@@ -0,0 +1,11 @@
+from numpy import *
+
+val = linspace(10,20,2)
+val = arange(10,20,2)
+val = logspace(10,20,2)
+val = ones(10)
+
+# for x in range(0,len(val)):
+#     print(val[x],end=" ")
+for x in val:
+    print(x,end=" ")
