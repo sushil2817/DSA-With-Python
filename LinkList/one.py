7d14eb21b93a3c909abe65ee8226dac6d85e11d9
sushil2817 <tony281723@gmail.com>
2026-02-18 12:06:59 +0530

    Create DB LL in py

--- LinkList/one.py
+++ LinkList/one.py
@@ -4,22 +4,27 @@ class Node:
         self.next = None
         self.prev = None
 
+
 class DoublyLL:
     def __init__(self):
         self.head = None
+
 
     def insertAtEnd(self,value):
         temp = Node(value)
         if(self.head == None):
             self.head = temp
             return
-        
+
+
         t = self.head
         while (t.next != None):
             t = t.next
 
+
         t.next = temp
         temp.prev = t
     
+
     def printDLL(self):
         while
